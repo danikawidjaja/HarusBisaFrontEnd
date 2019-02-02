22f23e6d6deb4ddc1739ebe63b42124b765b87c9
Danika Widjaja <danika.dew@utexas.edu>
2019-02-01 19:02:54 -0600

    not found page edited

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,9 +1,18 @@
 import React from "react";
 import "./NotFound.css";
 import {Link} from "react-router-dom";
+import pic from './hminianeh.png';
+import { Button} from "react-bootstrap";
 
 export default () =>
   <div className="NotFound">
-    <h3>Sorry, page not found!</h3>
-    <Link to="/" className="App-link"> Return home </Link>
+  	<div className='image'>
+  		<img src ={pic} />
+  	</div>
+  	<div className ='text'>
+  		<h1> hmm.. ini aneh </h1>
+  		<p> Terjadi kesalahan halaman yang anda minta </p>
+  		<br/>
+  		<Button className='n-button'> Kembali ke Home </Button>
+  	</div>
   </div>;
