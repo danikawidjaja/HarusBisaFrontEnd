4ad8e3716e4afcdc69e3e97a269d83113d5b8438
Danika Widjaja <danika.dew@utexas.edu>
2019-02-15 15:53:46 -0600

    button functions done. Working on the stylign

--- src/containers/FAQ.js
+++ src/containers/FAQ.js
@@ -19,44 +19,28 @@ export default class FAQ extends Component {
       role: value
     });
   }
+
+  informationDisplay(){
+    if (this.state.role === 'faculty'){
+      return(<Faculty/>)
+    }
+    else if (this.state.role === 'student'){
+      return (<Student/>)
+    }
+  }
   render() {
     return (
       <div className='faq'>
         <div className='text'> 
           <h1> F.A.Q </h1>
           <p> Informasi mengenai HarusBisa dan beberapa hal pertanyaan yang sering diajukan oleh pengguna </p>
-          <div className='buttons'>
-            <ToggleButtonGroup className='buttons' name='role'type='radio' defaultValue={'faculty'} onChange={this.handleChangeRole}>
-              <ToggleButton className='button' value='faculty' defaultChecked> Dosen </ToggleButton>
-              <ToggleButton className='button' value='student'> Mahasiswa </ToggleButton>
-            </ToggleButtonGroup>
-
-            {/*
-            <label className='button'>
-              <input
-                type='radio'
-                value='faculty'
-                checked={this.state.role === "faculty"}
-                onChange={this.handleChangeRole}
-              />
-              Dosen
-            </label>
-
-            <label className='button'>
-              <input
-                type='radio'
-                value='student'
-                checked={this.state.role === "student"}
-                onChange={this.handleChangeRole}
-              />
-              Mahasiswa
-            </label>
-
-          */}
-          </div>
+          <ToggleButtonGroup className='buttons' name='role'type='radio' defaultValue={'faculty'} onChange={this.handleChangeRole}>
+            <ToggleButton className='button' value='faculty' defaultChecked> Dosen </ToggleButton>
+            <ToggleButton className='button' value='student'> Mahasiswa </ToggleButton>
+          </ToggleButtonGroup>
         </div>
 
-        <Faculty/>
+        {this.informationDisplay()}
 
         <Footer/>
       </div>     
@@ -76,7 +60,7 @@ class Faculty extends Component{
         </div>
         
         <div className='right'>
-          <h3> Change Course Settings </h3>
+          <h3> Faculty </h3>
           <p> lorem ipsum </p>
         </div>
 
@@ -87,18 +71,46 @@ class Faculty extends Component{
 
 class Student extends Component{
 
+  constructor(props){
+    super(props);
+    this.state={
+      value:1
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(value, event) {
+    this.setState({
+      value
+    });
+  }
+
+  information(){
+    if (this.state.value === 1){
+      return(<p> This is how you change course setting </p>)
+    }
+    else if (this.state.value === 2){
+      return (<p> This is how you create a new course </p>)
+    }
+    else if (this.state.value === 3){
+      return(<p> This is how you create a new course in HarusBisa </p>)
+    }
+  }
+
   render(){
     return(
       <div className='info'>
-        <div className='left'>
-          <Button> Change Course Settings </Button>
-          <Button> Create a New Course </Button>
-          <Button> Creating a course in HarusBisa  </Button>
-        </div>
+        {/*<div className='left'>*/}
+          <ToggleButtonGroup className='left' name='information' type='radio' defaultValue={1} onChange={this.handleChange}>
+            <ToggleButton className='i_button' value={1} defaultChecked> Change Course Setting </ToggleButton>
+            <ToggleButton className='i_button' value={2}> Create a New Course </ToggleButton>
+            <ToggleButton className='i_button' value={3}> Creating a course in HarusBisa </ToggleButton>
+          </ToggleButtonGroup>
+        {/*</div>*/}
         
         <div className='right'>
-          <h3> Change Course Settings </h3>
-          <p> lorem ipsum </p>
+          <h3> Student </h3>
+          {this.information()}
         </div>
 
       </div>
