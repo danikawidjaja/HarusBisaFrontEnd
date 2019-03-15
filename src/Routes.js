fa8571eafbc6310de7ad9f3e1e2385789e9d3fd4
Danika Widjaja <danika.dew@utexas.edu>
2019-03-15 11:04:43 -0500

    refactoring ProfileAvatar

--- src/Routes.js
+++ src/Routes.js
@@ -7,7 +7,6 @@ import NotFound from "./containers/NotFound/NotFound";
 import Courses from './containers/Courses';
 import Lectures from './containers/Lectures';
 import AppliedRoute from './components/AppliedRoute';
-import Profile from './containers/Profile';
 import Questions from './containers/Questions';
 import CourseContent from './containers/CourseContent';
 import TermsAndConditions from './containers/TermsAndConditions/TermsAndConditions';
@@ -24,7 +23,6 @@ export default ({ childProps }) =>
         <AppliedRoute path ="/login" exact component = {Login} props={childProps} />
         <AppliedRoute path="/courses" component ={Courses} props={childProps}/>
         <AppliedRoute path='/dashboard' component={Dashboard} props={childProps}/>
-        <AppliedRoute path='/profile' component = {Profile} props={childProps} />
         <AppliedRoute path='/questions' component = {Questions} props={childProps} />
         <AppliedRoute path='/coursecontent' component={CourseContent} props={childProps}/>
         <AppliedRoute path='/termsandconditions' component={TermsAndConditions} props={childProps}/>
