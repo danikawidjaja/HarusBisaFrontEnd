a68f596e28bb4a0b6184a543ecaa30ed025063c6
Danika Widjaja <danika.dew@utexas.edu>
2019-04-08 16:46:30 -0500

    footer new design

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -6,39 +6,38 @@ import twitter from './twitter.png';
 import instagram from './instagram.png';
 import { Link } from "react-router-dom";
 import Logo from '../Logo/Logo';
-
+ 
 export default class Footer extends Component {
 
   render() {
     return (
-        <div>
         <div className='footer'>
-          <Logo size='logo' color='white' background='trans' padding={false} style={{width:'8vw',height:'25vh',margin:'auto'}}/>
-          <div className='f-about'>
-            <p> HarusBisa menyediakan alat untuk para insturktur memberikan lecture yang berkualitas. menggunakan alat yang canggih dan dapat bisa memudahkan secara GRATIS sampai ke seluruh pelosok Indonesia. </p>
-            <br/> <br/>
-            <p> Copyright PT. HarusBisa Indonesia | HarusBisa 2018 </p>
-          </div>
+          <div className='content'>
+            <div className='f-about'>
+              <p> HarusBisa menyediakan alat untuk para insturktur memberikan lecture yang berkualitas. menggunakan alat yang canggih dan dapat bisa memudahkan secara GRATIS sampai ke seluruh pelosok Indonesia. </p>
+              <br/> <br/>
+              <p> Copyright PT. HarusBisa Indonesia | HarusBisa 2018 </p>
+            </div>
 
-          <div className='links'>
-            <Link to='/' className='f-link'> Home </Link>
-            <Link to='/' className='f-link'> Kenapa <br/> HarusBisa? </Link>
-            <Link to='/' className='f-link'> Pricing </Link>
-          </div>
+            <div className='links'>
+              <Link to='/' className='f-link'> Home </Link>
+              <Link to='/' className='f-link'> Kenapa <br/> HarusBisa? </Link>
+              <Link to='/' className='f-link'> Pricing </Link>
+            </div>
 
-          <div className='links'>
-            <Link to='/faq' className='f-link'> FAQ </Link>
-            <Link to='/termsandconditions' className='f-link'> Syarat dan <br/> Ketentuan </Link>
-            <Link to='/contactus' className='f-link'> Hubungi Kami </Link>
-          </div>
+            <div className='links'>
+              <Link to='/faq' className='f-link'> FAQ </Link>
+              <Link to='/termsandconditions' className='f-link'> Syarat dan <br/> Ketentuan </Link>
+              <Link to='/contactus' className='f-link'> Hubungi Kami </Link>
+            </div>
 
-          <div className='socialmedia'>
-            <Link to="/"><img class="socmed-i" src={facebook}/></Link>
-            <Link to="/"><img class="socmed-i" src={twitter}/></Link>
-            <Link to="/"><img class="socmed-i" src={instagram}/></Link>
-            <Link to="/"><img class="socmed-i" src={linkedin}/></Link>
+            <div className='socialmedia'>
+              <Link to="/"><img class="socmed-i" src={facebook}/></Link>
+              <Link to="/"><img class="socmed-i" src={twitter}/></Link>
+              <Link to="/"><img class="socmed-i" src={instagram}/></Link>
+              <Link to="/"><img class="socmed-i" src={linkedin}/></Link>
+            </div>
           </div>
-        </div>
         </div>
     );
   }
