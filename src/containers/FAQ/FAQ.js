14570330daa11b126a07208c75784d28747ece38
Danika Widjaja <danika.dew@utexas.edu>
2019-08-03 11:52:43 -0500

    faq done

--- src/containers/FAQ/FAQ.js
+++ src/containers/FAQ/FAQ.js
@@ -4,12 +4,100 @@ import { ToggleButtonGroup, ToggleButton} from "react-bootstrap";
 import Footer from '../Footer/Footer';
 import { Button } from "@material-ui/core";
 
+const proffaqInfo = [
+  {option:"General",
+  qa:[
+    {question: "Apa itu kelas?", answer: "Answer"},
+    {question: "Apa itu kelas?", answer: "Answer"},
+    {question: "Apa itu kelas?", answer: "Answer"},
+    {question: "Apa itu kelas?", answer: "Answer"}
+  ]
+  }, 
+  {option: "Semester Baru",
+  qa:[
+    {question: "Bagaimana membuat sesi?", answer: "Answer"},
+    {question: "Bagaimana membuat sesi?", answer: "Answer"},
+    {question: "Bagaimana membuat sesi?", answer: "Answer"},
+    {question: "Bagaimana membuat sesi?", answer: "Answer"}
+  ]
+  }, 
+  {option: "Sebelum Kelas",
+  qa:[
+    {question: "Bagaimana menambah pertanyaan?", answer: "Answer"},
+    {question: "Bagaimana menambah pertanyaan?", answer: "Answer"},
+    {question: "Bagaimana menambah pertanyaan?", answer: "Answer"},
+    {question: "Bagaimana menambah pertanyaan?", answer: "Answer"}
+  ]
+  }, 
+  {option: "Sedang Kelas",
+  qa:[
+    {question: "Bagaimana mengganti pertanyaan?", answer: "Answer"},
+    {question: "Bagaimana mengganti pertanyaan?", answer: "Answer"},
+    {question: "Bagaimana mengganti pertanyaan?", answer: "Answer"},
+    {question: "Bagaimana mengganti pertanyaan?", answer: "Answer"}
+  ]
+  }, 
+  {option: "Selesai Kelas",
+  qa:[
+    {question: "Dimana melihat nilai?", answer: "Answer"},
+    {question: "Dimana melihat nilai?", answer: "Answer"},
+    {question: "Dimana melihat nilai?", answer: "Answer"},
+    {question: "Dimana melihat nilai?", answer: "Answer"}
+  ]
+  }
+]
+
+const studfaqInfo = [
+  {option:"General",
+  qa:[
+    {question: "Apa itu kelas?", answer: "Answer stude"},
+    {question: "Apa itu kelas?", answer: "Answer student"},
+    {question: "Apa itu kelas?", answer: "Answer student"},
+    {question: "Apa itu kelas?", answer: "Answer student"}
+  ]
+  }, 
+  {option: "Semester Baru",
+  qa:[
+    {question: "Bagaimana membuat sesi?", answer: "Answer student"},
+    {question: "Bagaimana membuat sesi?", answer: "Answer student"},
+    {question: "Bagaimana membuat sesi?", answer: "Answer student"},
+    {question: "Bagaimana membuat sesi?", answer: "Answer student"}
+  ]
+  }, 
+  {option: "Sebelum Kelas",
+  qa:[
+    {question: "Bagaimana menambah pertanyaan?", answer: "Answer student"},
+    {question: "Bagaimana menambah pertanyaan?", answer: "Answer student"},
+    {question: "Bagaimana menambah pertanyaan?", answer: "Answer student"},
+    {question: "Bagaimana menambah pertanyaan?", answer: "Answer student"}
+  ]
+  }, 
+  {option: "Sedang Kelas",
+  qa:[
+    {question: "Bagaimana mengganti pertanyaan?", answer: "Answer student"},
+    {question: "Bagaimana mengganti pertanyaan?", answer: "Answer student"},
+    {question: "Bagaimana mengganti pertanyaan?", answer: "Answer student"},
+    {question: "Bagaimana mengganti pertanyaan?", answer: "Answer student"}
+  ]
+  }, 
+  {option: "Selesai Kelas",
+  qa:[
+    {question: "Dimana melihat nilai?", answer: "Answer student"},
+    {question: "Dimana melihat nilai?", answer: "Answer student"},
+    {question: "Dimana melihat nilai?", answer: "Answer student"},
+    {question: "Dimana melihat nilai?", answer: "Answer student"}
+  ]
+  }
+]
+
+
 export default class FAQ extends Component {
   constructor(props){
     super(props);
     this.state ={
-      role:'faculty',
+      role:null,
       search: "",
+      info: null
     }
     this.handleSearch = this.handleSearch.bind(this);
     this.handleChangeRole = this.handleChangeRole.bind(this);
@@ -17,19 +105,22 @@ export default class FAQ extends Component {
   async componentDidMount(){
     window.scrollTo(0, 0);
   }
-  handleChangeRole(value) {
-    this.setState({
-      role: value
+  async handleChangeRole(value) {
+    var info = [];
+    if (value === "faculty"){
+      info = proffaqInfo;
+    }
+    else if (value === "student"){
+      info = studfaqInfo;
+    }
+    await this.setState({
+      role: value,
+      info : info
     });
   }
 
   informationDisplay(){
-    if (this.state.role === 'faculty'){
-      return(<FacultyFAQ/>)
-    }
-    else if (this.state.role === 'student'){
-      return (<StudentFAQ/>)
-    }
+    return (<Information role={this.state.role} info={this.state.info}/>)
   }
 
   handleSearch(event){
@@ -45,7 +136,7 @@ export default class FAQ extends Component {
           <div className='content'> 
             <h1>Bantuan</h1>
             <p>Informasi mengenai harusbisa dan beberapa hal pertanyaan yang sering diajukan oleh pengguna</p>
-            <input type='text' placeholder='Cari bantuan' value={this.state.search} onChange={this.handleSearch}/>
+            <input className="searchbar" type='text' placeholder='Cari bantuan' value={this.state.search} onChange={this.handleSearch}/>
             <div className="row buttons">
               <div className="col">
                 <Button onClick={() => this.handleChangeRole("faculty")} className={this.state.role === "faculty" ? "button-selected button" : "button"}>Dosen</Button>
@@ -54,9 +145,11 @@ export default class FAQ extends Component {
                 <Button onClick={() => this.handleChangeRole("student")} className={this.state.role === "student" ? "button-selected button" : "button"}>Mahasiswa</Button>
               </div>
             </div>
-          </div>
 
-          {this.state.search !== "" ? this.informationDisplay() : null}
+            <div className="information">
+              {this.state.role && this.informationDisplay()}
+            </div>
+          </div>
 
         </div>
         <Footer/>
@@ -65,47 +158,60 @@ export default class FAQ extends Component {
   }
 }
 
-class FacultyFAQ extends Component{
+class Information extends Component{
   constructor(props){
     super(props);
     this.state={
-      value:1
+      value:0
     }
     this.handleChange = this.handleChange.bind(this);
+    this.renderButtons = this.renderButtons.bind(this);
+    this.information = this.information.bind(this);
   }
 
-  handleChange(value, event) {
+  handleChange(value) {
     this.setState({
       value
     });
   }
 
   information(){
-    if (this.state.value === 1){
-      return(<p> This is how you change course setting </p>)
+    var qas = this.props.info[this.state.value].qa;
+    var components = [];
+
+    for (let i=0; i<qas.length; i++){
+      var qa = qas[i]
+      components.push(
+        <div>
+          <h5>{qa.question}</h5>
+          <p>{qa.answer}</p>
+        </div>
+      )
     }
-    else if (this.state.value === 2){
-      return (<p> This is how you create a new course </p>)
-    }
-    else if (this.state.value === 3){
-      return(<p> This is how you create a new course in HarusBisa </p>)
-    }
+
+    return components;
   }
 
+  renderButtons(){
+    var components = [];
+    var options = this.props.info
+
+    for (let i=0; i<options.length; i++){
+      var option = options[i].option
+      components.push(<Button className={this.state.value === i ? "button button-selected" : "button"} onClick={()=>{this.handleChange(i)}}>{option}</Button>);
+    }
+
+    return components;
+  }
   render(){
     return(
-      <div className='info'>
-          <ToggleButtonGroup className='left' name='information' type='radio' defaultValue={1} onChange={this.handleChange}>
-            <ToggleButton className='button' value={1} defaultChecked> Change Course Setting </ToggleButton>
-            <ToggleButton className='button' value={2}> Create a New Course </ToggleButton>
-            <ToggleButton className='button' value={3}> Creating a course in HarusBisa </ToggleButton>
-          </ToggleButtonGroup>
-        
-        <div className='right'>
-          <h3> Faculty </h3>
+      <div className='row justify-content-end'>
+        <div className="col-3 sidebar">
+          {this.renderButtons()}
+        </div>
+        <div className="col-8">
           {this.information()}
         </div>
-
       </div>
     )
   }
